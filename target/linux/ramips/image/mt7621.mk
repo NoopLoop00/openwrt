7d273c018eaf93936d26ca8b8e47f3af26f44507
NoopLoop00 <53203656+NoopLoop00@users.noreply.github.com>
2023-12-07 10:51:16 +0800

    Update mt7621.mk

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -3014,7 +3014,7 @@ TARGET_DEVICES += zte_e8820s
 define Device/zte_e8820v2
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
-  IMAGE_SIZE := 16064k
+  IMAGE_SIZE := 327448k
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820V2
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb2 \
